08e1932fb72cc1b51feb239423a82008becab21e
佳欣 <bear920211@gmail.com>
2025-12-11 15:19:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,14 @@
-// 初始化 Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBhK6eruZ8MvzeC4K7gavQswmG1yhjc2Oo",
-    authDomain: "lost-and-1702f.firebaseapp.com",
-    projectId: "lost-and-1702f",
-    storageBucket: "lost-and-1702f.firebasestorage.app",
-    messagingSenderId: "1026381242748",
-    appId: "1:1026381242748:web:4b07b6c5d98b680ab504f9",
-    measurementId: "G-5RZYL5SGC0"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
-
+// ⚠️ 請確認 storageBucket 用的是 .appspot.com，不是 firebasestorage.app
 const firebaseConfig = {
   apiKey: "AIzaSyBhK6eruZ8MvzeC4K7gavQswmG1yhjc2Oo",
   authDomain: "lost-and-1702f.firebaseapp.com",
   projectId: "lost-and-1702f",
-  storageBucket: "lost-and-1702f.firebasestorage.app",
+  storageBucket: "lost-and-1702f.appspot.com",
   messagingSenderId: "1026381242748",
   appId: "1:1026381242748:web:4b07b6c5d98b680ab504f9"
 };
 
+// 初始化 Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
